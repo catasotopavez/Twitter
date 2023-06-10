fe45068be4da3e20ee6479835629ccbda22adef1
catasotopavez <catasotopavez@Catalinas-MacBook-Pro.local>
2023-06-10 18:07:00 -0400

    ver hashtag

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,24 @@
-class Tweet <ApplicationRecord
+class Tweet < ApplicationRecord
     belongs_to :user
-    belongs_to :user
+    has_many :tags
+
     attribute :hashtags
+
+    after_create :create_tags
+
+    def create_tags
+        # debugger
+        hashtags.split(',').each do |h|
+            hashtag = Hashtag.find_or_create_by name: h
+            Tag.create tweet: self, hashtag: hashtag
+        end
+    end
+
     def creation_date
         created_at.strftime("%e %b %Y")
+    end
+
+    def list_hashtags
+        tags.map{ |t| t.hashtag.name }.join(',')
     end
 end
